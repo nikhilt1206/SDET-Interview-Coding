1c999669c52717ad9d65c0640c00b5e5aafab164
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-08-24 23:49:46 +0530

    Added code for Swap two numbers using third variable and mathematical operation

---
+++ src/logicbuildingrevision/SwapTwoNumbersUsingMathematicalOperation.java
@@ -0,0 +1,14 @@
+package logicbuildingrevision;
+
+public class SwapTwoNumbersUsingMathematicalOperation {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        System.out.println("Before swapping : a = "+a+" and b = "+b);
+
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("After swapping : a = "+a+" and b = "+b);
+    }
+}
---
+++ src/logicbuildingrevision/SwapTwoNumbersUsingTempVariable.java
@@ -0,0 +1,15 @@
+package logicbuildingrevision;
+
+public class SwapTwoNumbersUsingTempVariable {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        System.out.println("Before swapping : a = "+a+" and b = "+b);
+
+        int temp;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After swapping : a = "+a+" and b = "+b);
+    }
+}
