64ecab89851cbaa0a8817af3c4ea58c343b220fc
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-08-26 00:20:43 +0530

    Added code for Reversing an Integer along with overflow and underflow condtion check

---
+++ src/logicbuildingrevision/ReverseInteger.java
@@ -0,0 +1,28 @@
+package logicbuildingrevision;
+
+import jdk.jfr.consumer.RecordedEvent;
+
+public class ReverseInteger {
+    public static void main(String[] args){
+        int number = 1234;
+        int reverseNumber = 0 ;
+        int lastDigit;
+        while(number!=0){
+            lastDigit = number % 10;
+            //Check for overflow
+            if(reverseNumber > Integer.MAX_VALUE / 10 || (reverseNumber == Integer.MAX_VALUE / 10 && lastDigit > 7)){
+                System.out.println(0);
+                System.exit(0);
+            }
+
+            //Check for underflow
+            if(reverseNumber < Integer.MIN_VALUE / 10 || (reverseNumber == Integer.MIN_VALUE / 10 && lastDigit < -8)){
+                System.out.println(0);
+                System.exit(0);
+            }
+            reverseNumber = (reverseNumber * 10) + lastDigit;
+            number = number / 10;
+        }
+        System.out.println(reverseNumber);
+    }
+}
