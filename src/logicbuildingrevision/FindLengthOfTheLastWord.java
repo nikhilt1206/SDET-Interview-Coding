d7bc9c6cca3703478851a583407e6635f3d98a86
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-08-31 19:37:47 +0530

    Added code for Finding Length of the Last Word in a String

---
+++ src/logicbuildingrevision/FindLengthOfTheLastWord.java
@@ -0,0 +1,26 @@
+package logicbuildingrevision;
+
+public class FindLengthOfTheLastWord {
+    public static void main(String[] args){
+        String input = "Hello World";
+        if(input==null|| input.isEmpty()){
+            System.out.println("Invalid input!!");
+            return;
+        }
+        //Remove leading and trailing spaces so we can start from the last character
+        input = input.trim();
+        int count = 0;
+        //Traverse from the last word
+        for(int i=input.length()-1;i>=0;i--){
+            if(input.charAt(i)!=' '){
+                count=count+1;
+            }
+            else{
+                if(count>0){
+                    System.out.println(count);
+                    break;
+                }
+            }
+        }
+    }
+}
