4a8387cff254b1d7cd24883d09875809a3d7fb0f
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-09-01 10:05:19 +0530

    Added code for finding even length words from a String

---
+++ src/logicbuildingrevision/PrintEvenWordsFromAString.java
@@ -0,0 +1,13 @@
+package logicbuildingrevision;
+
+public class PrintEvenWordsFromAString {
+    public static void main(String[] args){
+      String input = "Sky is blue and vast";
+      String[] dataArray = input.split(" ");
+      for(String word : dataArray){
+          if(word.length() % 2 == 0){
+              System.out.println(word);
+          }
+      }
+    }
+}
