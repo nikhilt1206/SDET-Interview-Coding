c2a33fb8ba95094a5e0cb18a8477988e03888f14
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-09-06 22:40:03 +0530

    Added code for Pattern Printing

---
+++ src/logicbuildingrevision/PatternPrinting.java
@@ -0,0 +1,20 @@
+package logicbuildingrevision;
+
+public class PatternPrinting {
+    public static void main(String[] args){
+        /*Print
+           1
+           1 2
+           1 2 3
+           1 2 3 4
+           1 2 3 4 5
+        */
+        int number = 5;
+        for(int row=1;row<=number;row++){ //to print rows
+            for(int col=1;col<=row;col++){ //to print columns
+                System.out.print(col+" ");
+            }
+            System.out.println("");
+        }
+    }
+}
