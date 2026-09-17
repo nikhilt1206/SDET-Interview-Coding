7baba57ff47d39d11f662cb4da412189f4927a98
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-09-17 16:31:17 +0530

    Added code for Printing Pyramid Pattern

---
+++ src/interviewcoding/PyramidPatternPrinting.java
@@ -0,0 +1,19 @@
+package interviewcoding;
+
+public class PyramidPatternPrinting {
+    public static void main(String[] args){
+        int n = 5;
+        for(int row=1;row<=n;row++){
+            for(int col=1;col<=row;col++){
+                System.out.print(col);
+            }
+            System.out.println("");
+        }
+        for(int row=n-1;row>=1;row--){
+            for(int col=1;col<=row;col++){
+                System.out.print(col);
+            }
+            System.out.println("");
+        }
+    }
+}
